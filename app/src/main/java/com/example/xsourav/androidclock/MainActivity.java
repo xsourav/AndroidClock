8ea50a471ff2789f76c1a952c2cd601083c747d1
Your Name <you@example.com>
2018-01-26 19:45:31 +0530

    set the ui

---
+++ app/src/main/java/com/example/xsourav/androidclock/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.xsourav.androidclock;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView=findViewById(R.id.time_display);
+        textView.setText("7:30");
+    }
+}
